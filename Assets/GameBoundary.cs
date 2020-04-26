d4f073363e80a2c27fcf869d5eeed4ed4578ee5b
Jake Deakin <jake.deakin@outlook.com>
2020-04-26 10:23:47 +1000

    Dynamic boundary collider
    The edge collider around the edge of the screen will now change size to match the resolution of the screen.

---
+++ Assets/GameBoundary.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoundary : MonoBehaviour
+{
+    private Camera maincam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        maincam = GameObject.Find("Main Camera").GetComponent<Camera>();
+
+        GenerateBoundaryCollider();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenerateBoundaryCollider()
+    {
+        Vector2[] boundaryPoints;
+        boundaryPoints = this.GetComponent<EdgeCollider2D>().points;
+        boundaryPoints[0] = maincam.ViewportToWorldPoint(new Vector3(0f, 0f, maincam.nearClipPlane));
+        boundaryPoints[1] = maincam.ViewportToWorldPoint(new Vector3(0f, 1f, maincam.nearClipPlane));
+        boundaryPoints[2] = maincam.ViewportToWorldPoint(new Vector3(1f, 1f, maincam.nearClipPlane));
+        boundaryPoints[3] = maincam.ViewportToWorldPoint(new Vector3(1f, 0f, maincam.nearClipPlane));
+        boundaryPoints[4] = maincam.ViewportToWorldPoint(new Vector3(0f, 0f, maincam.nearClipPlane));
+        this.GetComponent<EdgeCollider2D>().points = boundaryPoints;
+    }
+}
